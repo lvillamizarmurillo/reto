ee23d3a66d68fd0c7ca5215937ede0730fdc4db5
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-28 16:45:40 -0500

    feat: :sparkles: Intento de consumo de api
    Se intento hacer un crud mas ordenado, pero me sale error y procedo a realizarlo de forma distinta

--- uploads/app.php
+++ uploads/app.php
@@ -3,5 +3,19 @@
     require "../vendor/autoload.php";
     $dotenv = Dotenv\Dotenv::createImmutable("../")->load();
     $router = new \Bramus\Router\Router();
-    
+    $router->mount('/campers', function() use($router){
+        $router->post('/', '\App\crud@postAll');
+        $router->put('/', '\App\crud@putAll');
+        $router->get("/", '\App\crud@getAll');
+        $router->delete('/', function() { 
+            $cox = new \App\connect();
+            $_DATA = json_decode(file_get_contents("php://input"));
+            $res = $cox->con->prepare("DELETE FROM campers WHERE idCamper=:idCamper");
+            $res->bindParam(':idCamper', $_DATA->idCamper);
+            $res->execute();
+            print_r($res->rowCount());
+        });
+    });
+
+    $router->run();
 ?>
